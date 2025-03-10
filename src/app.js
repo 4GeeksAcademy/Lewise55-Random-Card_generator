698efd9fb66cdc6de96a2c25b893359315c5650f
Lewise55 <157831388+Lewise55@users.noreply.github.com>
2025-03-10 22:51:33 +0000

    Random Card Gen Final Commit

--- src/app.js
+++ src/app.js
@@ -8,9 +8,29 @@ import "./assets/img/4geeks.ico";
 window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
+  // start();
   deck();
   document.querySelector("#dealBtn").addEventListener("click", deck)
 };
+  // const start = () =>{
+  //   const back = ["\u{1F0A0}"];
+
+  //   let backCard = "";
+  //   for(const face of back){
+  //     backCard = back; 
+  //   }
+
+  //   let startCard= back[Math.floor(Math.random() * backCard.length)];  
+  //   const backElement = document.querySelector(".back");
+  //   backElement.innerHTML = startCard;
+
+  //   for(let i = 52; i > 0; i--){
+  //     if(i == 0){
+  //       console.log();
+  //     }
+  //   }
+  //   console.log("start")
+  // }
 
   const deck = () => {
     const spades = ["\u{1F0A1}", "\u{1F0A2}", "\u{1F0A3}", "\u{1F0A4}", "\u{1F0A5}", "\u{1F0A6}", "\u{1F0A7}", "\u{1F0A8}", "\u{1F0A9}", "\u{1F0AA}", "\u{1F0AB}", "\u{1F0AD}", "\u{1F0AE}"];
@@ -18,32 +38,36 @@ window.onload = function() {
     const diamonds = ["\u{1F0C1}", "\u{1F0C2}", "\u{1F0C3}", "\u{1F0C4}", "\u{1F0C5}", "\u{1F0C6}", "\u{1F0C7}", "\u{1F0C8}", "\u{1F0C9}", "\u{1F0CA}", "\u{1F0CB}", "\u{1F0CD}", "\u{1F0CE}"];
     const clubs = ["\u{1F0D1}", "\u{1F0D2}", "\u{1F0D3}", "\u{1F0D4}", "\u{1F0D5}", "\u{1F0D6}", "\u{1F0D7}", "\u{1F0D8}", "\u{1F0D9}", "\u{1F0DA}", "\u{1F0DB}", "\u{1F0DD}", "\u{1F0DE}"];
     const jokers = ["\u{1F0BF}"];
-    const back = ["\u{1F0A0}"];
-    
+     
     const fullDeck = [];
     for(const suit of [spades, hearts, diamonds, clubs, jokers]){
       for(const card of suit){
         fullDeck.push(card);
       }
-    }
-    
-    let randomCard = fullDeck[Math.floor(Math.random() *fullDeck.length)];
+    } 
+      let randomCard = fullDeck[Math.floor(Math.random() *fullDeck.length)];
+      
+      const deckElement = document.querySelector(".deck");
+      deckElement.innerHTML = randomCard;
 
-    const deckElement = document.querySelector(".deck");
-    deckElement.innerHTML = randomCard;
+         if(hearts.includes(randomCard) || diamonds.includes(randomCard)) {
+          deckElement.style.color = "red";
+      } else if (spades.includes(randomCard) || clubs.includes(randomCard)) {
+          deckElement.style.color = "black";
+      } else {
+          deckElement.style.color = ["black", "red"]; 
+      }
+      let deckEnd = 52;
 
-    if (hearts.includes(randomCard) || diamonds.includes(randomCard)) {
-      deckElement.style.color = "red";
-  } else if (spades.includes(randomCard) || clubs.includes(randomCard)) {
-      deckElement.style.color = "black";
-  } else {
-      deckElement.style.color = ["black", "red"]; 
-  }
+      for(let i = fullDeck.length; i < 0; i--){
+        deckEnd - 1;
+        if(deckEnd == 0);
+      }
+       
 
-    document.querySelector(".deck").innerHTML = randomCard;
+    document.querySelector(".deck").innerHTML = randomCard;  
 
     console.log("deck");
 
-  
 }
   
